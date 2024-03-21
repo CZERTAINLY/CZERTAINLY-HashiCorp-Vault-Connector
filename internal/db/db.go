20ad7b0d14f411895bad16e36a0a6a439a62027a
3keyroman <roman.cinkais@3key.company>
2024-03-21 18:30:58 +0100

    Update db.go

--- internal/db/db.go
+++ internal/db/db.go
@@ -34,7 +34,7 @@ func MigrateDB(config config.Config) {
 	log.Info("Migrating database")
 	// search_path=public&x-migrations-table=hvault_migrations migration table name and schema, migration table must be in public schema if we want to create schema automatically
 	connectionString := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?search_path=%s&x-migrations-table=hvault_migrations&sslmode=%s", config.Database.Username, config.Database.Password, config.Database.Host, config.Database.Port, config.Database.Name, config.Database.Schema, config.Database.SslMode)
-	log.Info("Connection string: " + connectionString)
+	// log.Info("Connection string: " + connectionString)
 	m, err := migrate.New(
 		"file://migrations",
 		connectionString,
